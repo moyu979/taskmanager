604e7f81d7731fadd52abcb77d95e6f1989d3fc6
moyu979 <moyu979@gmail.com>
2024-01-28 23:59:50 +0800

    try log

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,11 +9,13 @@ public class Main {
     private static Logger logger = LogManager.getLogger("liuyxlogger");
     private static Logger logger2 = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        //PropertyConfigurator.configure("src\\main\\resources\\log4j2.properties");
         logger.trace("hello liuyx log");
         logger.info("hello");
         logger.error("im an error");
+        logger.debug("im an debug");
         logger2.info("from logger2");
         logger2.error("im an error");
     }
